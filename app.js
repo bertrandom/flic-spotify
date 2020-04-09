f607e431710e0d187483f78119de88e509b767a4
Bertrand Fan <bertrand@fan.net>
2020-04-09 12:41:39 -0700

    add logging, switch to playlist

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const spotify = require('./lib/spotify');
+const logger = require('./lib/logger');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/', (req, res) => {
   if (req.body.secret === config.secret) {
-    console.log('triggered play');
+    logger.info('triggered play');
     spotify.play();
     return res.send('OK');
   }
+  logger.error('invalid secret');
   res.sendStatus(401);
 });
 
-app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
+app.listen(config.port, () => logger.info(`Server running on port ${config.port}`));
